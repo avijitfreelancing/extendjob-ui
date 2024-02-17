93bcc592dfeabc94db8b9ba164980bf68991a5b9
samantaavijit <samantaavijit.2000@gmail.com>
2024-02-17 21:37:01 +0530

    admin login complete

--- layout/AdminWrapper.js
+++ layout/AdminWrapper.js
@@ -1,15 +1,16 @@
 "use client";
 
 import MobileMenu from "@/components/header/MobileMenu";
-import UserHeader from "@/components/header/UserHeader";
 import CopyrightFooter from "@/layout/CopyrightFooter";
 import AdminSidebar from "@/components/header/AdminSidebar";
+import AdminHeader from "@/components/header/AdminHeader";
 
 const AdminWrapper = ({ children }) => {
   return (
     <div className="page-wrapper dashboard">
-      <UserHeader />
-      <MobileMenu />
+      <span className="header-span" />
+      <AdminHeader />
+      {/* <MobileMenu /> */}
       <AdminSidebar />
       {children}
       <CopyrightFooter />
