d95b143c6d73ec78b03753f27649eef97f83cca0
samantaavijit <samantaavijit.2000@gmail.com>
2024-03-17 11:51:38 +0530

    new deployed

--- helper/axios.js
+++ helper/axios.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:3005/api", //local
-  //   baseURL: "http://44.208.45.69/api", //PROD
+  // baseURL: "http://localhost:3005/api", //local
+  baseURL: "https://extendjob-api.vercel.app/api", //PROD
 });
 
 export default axios;
